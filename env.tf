9a32ae62ce26fc18cd380253b4fdf8b81d286d7b
Alfiia Barylenko <98765399+Heshercat@users.noreply.github.com>
2025-06-26 17:22:52 +0300

    Update env.tf

--- env.tf
+++ env.tf
@@ -1,8 +1,8 @@
 resource "scalr_environment" "env_tag" {
-  name                    = "env_tag_${formatdate("DDMMYYYY", timestamp())}"
+  name                    = "alfi-env_tag_${formatdate("DDMMYYYY", timestamp())}"
   tag_ids                 = [scalr_tag.auto_tag.id]
 }
 
 resource "scalr_environment" "env_no_tag" {
-  name                    = "env_no_tag_${formatdate("DDMMYYYY", timestamp())}"
+  name                    = "alfi-env_no_tag_${formatdate("DDMMYYYY", timestamp())}"
 }
